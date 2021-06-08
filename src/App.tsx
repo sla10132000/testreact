38b3aad3c95f4ac678726e1fd0d0a80769664875
sla <sla10132000@yahoo.co.jp>
2021-06-08 13:19:41 +0900

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TimeLineChart from './TimeLineChart'
 
 function App() {
   return (
     <div className="App">
+      <TimeLineChart/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
